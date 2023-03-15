1f087420ff1717c99670144f1dd9aac4d19305c5
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2023-03-15 03:19:42 +0300

    FoodDAO commit

---
+++ app/src/main/java/com/example/ncnutrition/data/FoodDAO.kt
@@ -0,0 +1,25 @@
+package com.example.ncnutrition.data
+
+import androidx.room.*
+import com.example.ncnutrition.model.Food
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FoodDAO {
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insert(food: Food)
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insert( vararg foods:Food)
+    @Update
+    suspend fun update(food: Food)
+    @Delete
+    suspend fun delete(food: Food)
+    @Query("select * from food ORDER BY Code asc")
+    fun getFoods():Flow<List<Food>>
+    @Query("select * from food where id = :id")
+    fun getFood(id:Int):Flow<Food>
+    @Query("select * from food where Food_Group_Code = :food_group_code")
+    fun getFoodsByFoodGroup(food_group_code:Int):Flow<List<Food>>
+    @Query("select * from food where Food_name LIKE :food_name LIMIT 5")
+    fun findFoodByName(food_name:String):Flow<List<Food>>
+}
