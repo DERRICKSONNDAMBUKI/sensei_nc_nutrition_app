ec92f5f8e45b11d0869976eb69394fca2aa9f26d
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2023-03-25 22:49:41 +0300

    ConditionViewModel.ktfoods logic  commit

--- app/src/main/java/com/example/ncnutrition/ui/conditions/viewModel/ConditionViewModel.kt
+++ app/src/main/java/com/example/ncnutrition/ui/conditions/viewModel/ConditionViewModel.kt
@@ -60,8 +60,48 @@ class ConditionViewModel(private val conditionDAO: ConditionDAO, private val foo
 
     private fun conditionFoods(condition: Condition): LiveData<List<Food>> {
         val liveDataFoodList = when (condition.name) {
-            "Cancer" -> joinLiveDataLists(getFoodsByNutrients("water_in_g", "low").asLiveData())
-
+            "Cancer" -> joinLiveDataLists(
+                getFoodsByNutrients("energy_in_kcal", "low").asLiveData(),
+                getFoodsByNutrients("beta_carotene_equivalent_in_mcg","rich").asLiveData(),
+                getFoodsByNutrients("se_in_mg","regular").asLiveData(),
+                foodDAO.getFoodsByFoodGroup(4).asLiveData(),
+                foodDAO.getFoodsByFoodGroup(5).asLiveData()
+            )
+            "Cardiovascular disease"->joinLiveDataLists(
+                getFoodsByNutrients("niacin_in_mcg","rich").asLiveData(),
+                getFoodsByNutrients("fibre_in_g","rich").asLiveData(),
+                getFoodsByNutrients("fat_in_g","low").asLiveData(),
+                getFoodsByNutrients("beta_carotene_equivalent_in_mcg","rich").asLiveData(),
+                getFoodsByNutrients("cholesterol_in_mg","low").asLiveData() // vitamin E
+            )
+// energy_in_kJ,
+//            energy_in_kcal,
+//            water_in_g,
+//            protein_in_g,
+//            fat_in_g,
+//            carbohydrate_available_in_g,
+//            fibre_in_g,
+//            ash_in_g,
+//            ca_in_mg,
+//            fe_in_mg,
+//            mg_in_mg,
+//            p_in_mg,
+//            k_in_mg,
+//            na_in_mg,
+//            zn_in_mg,
+//            se_in_mg,
+//            vit_a_rae_in_mcg,
+//            vit_a_re_in_mcg,
+//            retinol_in_mcg,
+//            beta_carotene_equivalent_in_mcg,
+//            thiamin_in_mcg,
+//            riboflavin_in_mcg,
+//            niacin_in_mcg,
+//            dietary_folate_eq_in_mcg,
+//            food_folate_in_mcg,
+//            vit_b12_in_mcg,
+//            vit_c_in_mcg,
+//            cholesterol_in_mg,
             else -> emptyFlow<List<Food>>().asLiveData()
         }
         return liveDataFoodList
@@ -140,85 +180,85 @@ class ConditionViewModel(private val conditionDAO: ConditionDAO, private val foo
 
         return when (nutrient) {
             "energy_in_kcal" -> return (when (level) {
-                "high" -> foodDAO.getRichEnergyFoods(q2)
+                "rich" -> foodDAO.getRichEnergyFoods(q2)
                 "low" -> foodDAO.getLowEnergyFoods(q2)
                 "regular" -> foodDAO.getRegularEnergyFoods(q1, q3)
                 else -> emptyFlow()
             })
             "water_in_g" -> return (when (level) {
-                "high" -> foodDAO.getRichWaterInG(q2)
+                "rich" -> foodDAO.getRichWaterInG(q2)
                 "low" -> foodDAO.getLowWaterInG(q2)
                 "regular" -> foodDAO.getRegularWaterInG(q1, q3)
                 else -> emptyFlow()
             })
             "protein_in_g" -> return (when (level) {
-                "high" -> foodDAO.getRichProteinInG(q2)
+                "rich" -> foodDAO.getRichProteinInG(q2)
                 "low" -> foodDAO.getLowProteinInG(q2)
                 "regular" -> foodDAO.getRegularProteinInG(q1, q3)
                 else -> emptyFlow()
             })
             "fat_in_g" -> return (when (level) {
-                "high" -> foodDAO.getRichFatInG(q2)
+                "rich" -> foodDAO.getRichFatInG(q2)
                 "low" -> foodDAO.getLowFatInG(q2)
                 "regular" -> foodDAO.getRegularFatInG(q1, q3)
                 else -> emptyFlow()
             })
             "carbohydrate_available_in_g" -> return (when (level) {
-                "high" -> foodDAO.getRichCarbohydrateAvailableInG(q2)
+                "rich" -> foodDAO.getRichCarbohydrateAvailableInG(q2)
                 "low" -> foodDAO.getLowCarbohydrateAvailableInG(q2)
                 "regular" -> foodDAO.getRegularCarbohydrateAvailableInG(q1, q3)
                 else -> emptyFlow()
             })
             "fibre_in_g" -> return (when (level) {
-                "high" -> foodDAO.getRichFibreInG(q2)
+                "rich" -> foodDAO.getRichFibreInG(q2)
                 "low" -> foodDAO.getLowFibreInG(q2)
                 "regular" -> foodDAO.getRegularFibreInG(q1, q3)
                 else -> emptyFlow()
             })
             "ash_in_g" -> return (when (level) {
-                "high" -> foodDAO.getRichAshInG(q2)
+                "rich" -> foodDAO.getRichAshInG(q2)
                 "low" -> foodDAO.getLowAshInG(q2)
                 "regular" -> foodDAO.getRegularAshInG(q1, q3)
                 else -> emptyFlow()
             })
             "ca_in_mg" -> return (when (level) {
-                "high" -> foodDAO.getRichCaInMg(q2)
+                "rich" -> foodDAO.getRichCaInMg(q2)
                 "low" -> foodDAO.getLowCaInMg(q2)
                 "regular" -> foodDAO.getRegularCaInMg(q1, q3)
                 else -> emptyFlow()
             })
             "fe_in_mg" -> return (when (level) {
-                "high" -> foodDAO.getRichFeInMg(q2)
+                "rich" -> foodDAO.getRichFeInMg(q2)
                 "low" -> foodDAO.getLowFeInMg(q2)
                 "regular" -> foodDAO.getRegularFeInMg(q1, q3)
                 else -> emptyFlow()
             })
             "mg_in_mg" -> return (when (level) {
-                "high" -> foodDAO.getRichMgInMg(q2)
+                "rich" -> foodDAO.getRichMgInMg(q2)
                 "low" -> foodDAO.getLowMgInMg(q2)
                 "regular" -> foodDAO.getRegularMgInMg(q1, q3)
                 else -> emptyFlow()
             })
             "p_in_mg" -> return (when (level) {
-                "high" -> foodDAO.getRichPInMg(q2)
+                "rich" -> foodDAO.getRichPInMg(q2)
                 "low" -> foodDAO.getLowPInMg(q2)
                 "regular" -> foodDAO.getRegularPInMg(q1, q3)
                 else -> emptyFlow()
             })
             "k_in_mg" -> return (when (level) {
-                "high" -> foodDAO.getRichKInMg(q2)
+                "rich" -> foodDAO.getRichKInMg(q2)
                 "low" -> foodDAO.getLowKInMg(q2)
                 "regular" -> foodDAO.getRegularKInMg(q1, q3)
                 else -> emptyFlow()
             })
             "na_in_mg" -> return (when (level) {
-                "high" -> foodDAO.getRichNaInMg(q2)
+                "rich" -> foodDAO.getRichNaInMg(q2)
                 "low" -> foodDAO.getLowNaInMg(q2)
                 "regular" -> foodDAO.getRegularNaInMg(q1, q3)
                 else -> emptyFlow()
             })
             "zn_in_mg" -> return (when (level) {
-                "high" -> foodDAO.getRichZnInMg(q2)
+                "rich" -> foodDAO.getRichZnInMg(q2)
                 "low" -> foodDAO.getLowZnInMg(q2)
                 "regular" -> foodDAO.getRegularZnInMg(q1, q3)
                 else -> emptyFlow()
@@ -230,73 +270,73 @@ class ConditionViewModel(private val conditionDAO: ConditionDAO, private val foo
                 else -> emptyFlow()
             })
             "vit_a_rae_in_mcg" -> return (when (level) {
-                "high" -> foodDAO.getRichVitARaeInMcg(q2)
+                "rich" -> foodDAO.getRichVitARaeInMcg(q2)
                 "low" -> foodDAO.getLowVitARaeInMcg(q2)
                 "regular" -> foodDAO.getRegularVitARaeInMcg(q1, q3)
                 else -> emptyFlow()
             })
             "vit_a_re_in_mcg" -> return (when (level) {
-                "high" -> foodDAO.getRichVitAReInMcg(q2)
+                "rich" -> foodDAO.getRichVitAReInMcg(q2)
                 "low" -> foodDAO.getLowVitAReInMcg(q2)
                 "regular" -> foodDAO.getRegularVitAReInMcg(q1, q3)
                 else -> emptyFlow()
             })
             "retinol_in_mcg" -> return (when (level) {
-                "high" -> foodDAO.getRichRetinolInMcg(q2)
+                "rich"-> foodDAO.getRichRetinolInMcg(q2)
                 "low" -> foodDAO.getLowRetinolInMcg(q2)
                 "regular" -> foodDAO.getRegularRetinolInMcg(q1, q3)
                 else -> emptyFlow()
             })
             "beta_carotene_equivalent_in_mcg" -> return (when (level) {
-                "high" -> foodDAO.getRichBetaCaroteneEquivalentInMcg(q2)
+                "rich"-> foodDAO.getRichBetaCaroteneEquivalentInMcg(q2)
                 "low" -> foodDAO.getLowBetaCaroteneEquivalentInMcg(q2)
                 "regular" -> foodDAO.getRegularBetaCaroteneEquivalentInMcg(q1, q3)
                 else -> emptyFlow()
             })
             "thiamin_in_mcg" -> return (when (level) {
-                "high" -> foodDAO.getRichThiaminInMcg(q2)
+                "rich" -> foodDAO.getRichThiaminInMcg(q2)
                 "low" -> foodDAO.getLowThiaminInMcg(q2)
                 "regular" -> foodDAO.getRegularThiaminInMcg(q1, q3)
                 else -> emptyFlow()
             })
             "riboflavin_in_mcg" -> return (when (level) {
-                "high" -> foodDAO.getRichRiboflavinInMcg(q2)
+                "rich" -> foodDAO.getRichRiboflavinInMcg(q2)
                 "low" -> foodDAO.getLowRiboflavinInMcg(q2)
                 "regular" -> foodDAO.getRegularRiboflavinInMcg(q1, q3)
                 else -> emptyFlow()
             })
             "niacin_in_mcg" -> return (when (level) {
-                "high" -> foodDAO.getRichNiacinInMcg(q2)
+                "rich" -> foodDAO.getRichNiacinInMcg(q2)
                 "low" -> foodDAO.getLowNiacinInMcg(q2)
                 "regular" -> foodDAO.getRegularNiacinInMcg(q1, q3)
                 else -> emptyFlow()
             })
             "dietary_folate_eq_in_mcg" -> return (when (level) {
-                "high" -> foodDAO.getRichDietaryFolateEqInMcg(q2)
+                "rich" -> foodDAO.getRichDietaryFolateEqInMcg(q2)
                 "low" -> foodDAO.getLowDietaryFolateEqInMcg(q2)
                 "regular" -> foodDAO.getRegularDietaryFolateEqInMcg(q1, q3)
                 else -> emptyFlow()
             })
             "food_folate_in_mcg" -> return (when (level) {
-                "high" -> foodDAO.getRichFoodFolateInMcg(q2)
+                "rich" -> foodDAO.getRichFoodFolateInMcg(q2)
                 "low" -> foodDAO.getLowFoodFolateInMcg(q2)
                 "regular" -> foodDAO.getRegularFoodFolateInMcg(q1, q3)
                 else -> emptyFlow()
             })
             "vit_b12_in_mcg" -> return (when (level) {
-                "high" -> foodDAO.getRichVitB12InMcg(q2)
+                "rich" -> foodDAO.getRichVitB12InMcg(q2)
                 "low" -> foodDAO.getLowVitB12InMcg(q2)
                 "regular" -> foodDAO.getRegularVitB12InMcg(q1, q3)
                 else -> emptyFlow()
             })
             "vit_c_in_mcg" -> return (when (level) {
-                "high" -> foodDAO.getRichWaterInG(q2)
+                "rich"-> foodDAO.getRichWaterInG(q2)
                 "low" -> foodDAO.getLowWaterInG(q2)
                 "regular" -> foodDAO.getRegularVitCInMcg(q1, q3)
                 else -> emptyFlow()
             })
             "cholesterol_in_mg" -> return (when (level) {
-                "high" -> foodDAO.getRichCholesterolInMg(q2)
+                "rich" -> foodDAO.getRichCholesterolInMg(q2)
                 "low" -> foodDAO.getLowCholesterolInMg(q2)
                 "regular" -> foodDAO.getRegularCholesterolInMg(q1, q3)
                 else -> emptyFlow()
