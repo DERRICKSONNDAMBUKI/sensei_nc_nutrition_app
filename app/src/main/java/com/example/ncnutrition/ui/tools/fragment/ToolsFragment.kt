30f5025abf09d8a079f70ee30bf59285dab2b036
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2023-04-05 14:53:41 +0300

    ui  commit

--- app/src/main/java/com/example/ncnutrition/ui/tools/fragment/ToolsFragment.kt
+++ app/src/main/java/com/example/ncnutrition/ui/tools/fragment/ToolsFragment.kt
@@ -4,27 +4,61 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.ncnutrition.NCNutritionApplication
 import com.example.ncnutrition.databinding.FragmentToolsBinding
 
 class ToolsFragment : Fragment() {
 
-    private var _binding : FragmentToolsBinding? = null
+    private val viewModel: ToolsViewModel by activityViewModels {
+        ToolsViewModelFactory(
+            (activity?.application as NCNutritionApplication).database.foodDao()
+        )
+    }
+
+    private var _binding: FragmentToolsBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var viewModel: ToolsViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentToolsBinding.inflate(inflater,container,false)
-        return  binding.root
+        _binding = FragmentToolsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.measureBMI.setOnClickListener {
+            measureBMI()
+        }
+    }
+
+    private fun isEntryValid(): Boolean {
+        return viewModel.isEntryValid(
+            weight = binding.editTextNumberDecimalWeight.text.toString().toDoubleOrNull(),
+            height = binding.editTextNumberDecimalHeight.text.toString().toDoubleOrNull()
+        )
+    }
+
+    private fun measureBMI() {
+        if (isEntryValid()) {
+            val weight = binding.editTextNumberDecimalWeight.text.toString().toDouble()
+            val height = binding.editTextNumberDecimalHeight.text.toString().toDouble()
+            binding.apply {
+                textViewBMIResult.visibility = View.VISIBLE
+                textViewBMIResult.text = viewModel.getBMI(weight, height)
+                editTextNumberDecimalHeight.text.clear()
+                editTextNumberDecimalWeight.text.clear()
+            }
+
+        } else {
+            binding.textViewBMIResult.visibility = View.INVISIBLE
+            Toast.makeText(context, "Invalid weight/height", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/ncnutrition/ui/tools/fragment/ToolsViewModel.kt
+++ app/src/main/java/com/example/ncnutrition/ui/tools/fragment/ToolsViewModel.kt
@@ -1,7 +1,68 @@
 package com.example.ncnutrition.ui.tools.fragment
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.ncnutrition.data.dao.FoodDAO
+import kotlin.math.pow
 
-class ToolsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ToolsViewModel(private val foodDAO: FoodDAO) : ViewModel() {
+
+    fun getBMI(weight: Double, height: Double): String {
+
+        val category: String = when (weight / (height / 100.0).pow(2.0)) {
+            in 0.0..16.0 -> "Underweight severe thinness 😒"
+            in 16.0..16.99 -> "Underweight moderate thinness 😒"
+            in 17.0..18.49 -> "Underweight mild thinness 😒"
+            in 18.5..24.99 -> "Normal Weight 😍"
+            in 25.0..29.9 -> "Overweight or pre-obese 😮"
+            in 30.0..34.99 -> "Obese class I 😱"
+            in 35.0..39.99 -> "Obese class II 😱"
+            else -> "Obese class III 😱"
+        }
+
+//
+//        if (bmi < 16.0) {
+//            category = "Underweight severe thinness 😒"
+//        } else if (bmi in 16.0..16.99) {
+//            category = "Underweight moderate thinness 😒"
+//        } else if (bmi in 17.0..18.49) {
+//            category = "Underweight mild thinness 😒";
+//        } else if (bmi in 18.5..24.99) {
+//            category = "Normal Weight 😍";
+//        } else if (bmi in 25.0..29.9) {
+//            category = "Overweight or pre-obese 😮";
+//        } else if (bmi in 30.0..34.99) {
+//            category = "Obese class I 😱";
+//        } else if (bmi in 35.0..39.99) {
+//            category = "Obese class II 😱";
+//        } else if (bmi >= 40.0) {
+//            category = "Obese class III 😱";
+//        } else {
+//            category = "please input weight and height"
+//        }
+
+        return category
+    }
+
+    fun isEntryValid(weight: Double?, height: Double?): Boolean {
+        if (weight?.isNaN() != false || weight.toString().isEmpty() || weight.toString().isBlank()
+            ||
+            height?.isNaN() != false || height.toString().isBlank() || height.toString().isEmpty()
+        ) {
+            return false
+        }
+        return true
+    }
+
+
+}
+
+class ToolsViewModelFactory(private val foodDAO: FoodDAO) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ToolsViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST") return ToolsViewModel(foodDAO) as T
+        } else {
+            throw IllegalArgumentException("Unknown viewModel class")
+        }
+    }
 }
