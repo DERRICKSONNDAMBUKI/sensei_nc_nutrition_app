1659dc31ec069b91690b1745a625fcbc86f1f493
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2023-04-22 17:24:10 +0300

    BMI tools commit

--- app/src/main/java/com/example/ncnutrition/ui/tools/fragment/ToolsFragment.kt
+++ app/src/main/java/com/example/ncnutrition/ui/tools/fragment/ToolsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.ncnutrition.NCNutritionApplication
